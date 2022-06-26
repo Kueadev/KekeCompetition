5c204f0c87ac2d82ed0d3cadce160560e774c319
shiftwolf <54272779+shiftwolf@users.noreply.github.com>
2022-06-26 14:04:24 +0200

    added v2 of genetic + astar

---
+++ Keke_JS/agents/genetic2Astar_AGENT.js
@@ -0,0 +1,106 @@
+"use strict";
+
+// get imports (local)
+const simjs = require('../js/simulation');
+const Generation = require('../js/generation')
+const Creature = require('../js/creature')
+const astar = require('../js/astarKeke')
+
+// deep copies objects
+var cloneDeep = require('lodash.clonedeep');
+
+const generations = []
+
+// Runs (or amount of creatures) in a generation
+const RUNS = 100
+
+// Max amount of steps a creature takes
+const STEPS = 80
+
+
+function prepareNextGeneration() {
+  // Create next generation
+  generations.push(new Generation(RUNS))
+  const nextGeneration = generations[generations.length - 1]
+  const prevGeneration = generations[generations.length - 2]
+
+  // Breeding process
+  prevGeneration.evaluateCreatures().createProbabilityPool()
+  nextGeneration.creatures = prevGeneration.breedCreaturesUsingProbabilityPool()
+}
+
+function runCreature(state, generation) {
+  const creature = generation.creatures[generation.runsCompleted]
+  let move
+  for (let i = 0; i < creature.actions.length; i++) {
+    move = simjs.nextMove(creature.actions[i], state)
+    if (!move.next_state.players.length) {
+      // Try to find a path using the Astar algorithm just before dying
+      let path = astar(state)
+      if (path.length) {
+        const revisedActions = creature.actions.slice(0, i + 1).concat(path)
+        creature.actions = revisedActions
+        return {
+          isWin: true,
+          creature: creature,
+          solutionLength: i + path.length
+        }
+      }
+
+      // die
+      creature.diedAt = i
+      creature.evaluateStateFitness(move.next_state)
+      break
+    }
+    creature.evaluateStateFitness(move.next_state)
+    if (move.won) return {
+      isWin: true,
+      creature: creature,
+      solutionLength: i + 1
+    };
+  }
+  return {
+    isWin: false,
+    creature: creature
+  }
+}
+
+// This function is called for every generation.
+function nextGeneration(initState) {
+  // Determine current generation.
+  const currentGen = generations[generations.length - 1]
+
+  while (currentGen.runsCompleted < RUNS) {
+    // Hard copy of the state
+    const state = cloneDeep(initState)
+
+    // Run a creature and check if successful
+    const runInformation = runCreature(state, currentGen)
+    if (runInformation.isWin) return runInformation.creature.actions.slice(0, runInformation.solutionLength);
+    currentGen.runsCompleted += 1
+  }
+  prepareNextGeneration()
+
+  // return a sequence of actions if successful or empty list if unsuccessful.
+  return [];
+}
+
+// eslint-disable-next-line no-unused-vars
+function initStack(initState) {
+  // Here we initialize our agent before running.
+  // We reset the generations array, in case it's not empty from running the previous level.
+  // We add the first generation to the array. Further generations will be added on demand during iteration.
+  generations.length = 0
+  generations.push(new Generation(RUNS))
+  // We add our creatures whose genetic makeup is completely random to the first generation.
+  for (let i = 0; i < RUNS; i++) {
+    generations[0].creatures.push(new Creature(STEPS).addRandomActions(STEPS))
+  }
+}
+
+// These functions are exported, so that the simulator can access our agent.
+module.exports = {
+  step(initState) { return nextGeneration(initState); },
+  init(initState) { initStack(initState); },
+  best_sol() { return generations[generations.length - 1].creatures[0].actions; }
+};
---
+++ Keke_JS/js/astarKeke.js
@@ -0,0 +1,170 @@
+// BABA IS Y'ALL SOLVER - BLANK TEMPLATE
+// Version 1.0
+// Code by Milk 
+
+"use strict";
+
+//get imports (NODEJS)			//access the game states and simulation
+var astar = require('./astar')
+
+function initSearchGrid(state){
+	// initialize grid of the same size as map
+	// 1 = empty (pass-through)
+	// 0 = walls (not pass-through or kill the player)
+
+	const numOfRows = state.obj_map.length
+	const numOfColumns = state.obj_map[0].length
+	const grid = new Array(numOfRows).fill(1).map(() => new Array(numOfColumns).fill(1))
+
+	// set borders = walls
+	grid[0] = new Array(numOfColumns).fill(0)				// top
+	grid[numOfRows-1] = new Array(numOfColumns).fill(0)		// bottom
+	for (let i = 1; i < numOfRows-1; i++) {
+		grid[i][0] = 0										// left
+		grid[i][numOfColumns-1] = 0							// right
+	}
+
+	// unoverlaps = keywords, stop-objects, push-objects
+	for (let object of state.unoverlaps) {
+		grid[object.y][object.x] = 0
+	}
+	// kill-objects
+	for (let object of state.killers) {
+		grid[object.y][object.x] = 0
+	}
+	// sink-objects
+	for (let object of state.sinkers) {
+		grid[object.y][object.x] = 0
+	}
+
+	// only if melt trait exists: hot-objects aren't pass-through
+	const hasMeltRule = checkRuleExists('melt', parseRules(state.rules))
+	if (hasMeltRule) {
+		// state.featured: all objects that are either melt or hot
+		if (state.featured.length > 0) {
+			for (let object of state.featured) {
+				if (object.feature === 'hot') {
+					grid[object.y][object.x] = 0
+				}
+			}
+		}
+	}
+
+	// make sure players are not 'walls' (might be redundant)
+	const players = state.players
+	for (let player of players) {
+		grid[player.y][player.x] = 1
+	}
+
+	return grid
+}
+
+
+function parseRules(strRules) {
+	const rules = []
+	for (const rule of strRules) {
+		// A rule is a string like "baba-is-melt"
+		const splitRule = rule.split('-')
+		rules.push({
+			object: splitRule[0],
+			trait: splitRule[2]
+		})
+	}
+	return rules
+}
+
+
+function checkRuleExists(checkRule, rules) {
+	for (let rule of rules) {
+		if (rule.trait === checkRule) {
+			return true
+		}
+	}
+	return false
+}
+
+
+function convertResultToActions(astarResult) {
+	const actions = []
+
+	for (let node of astarResult) {
+		// horizontal move
+		if (node.x === node.parent.x) {
+			if (node.y > node.parent.y) {
+				actions.push('right')
+			} else {
+				actions.push('left')
+			}
+		} else {
+		// vertical move
+			if (node.x > node.parent.x) {
+				actions.push('down')
+			} else {
+				actions.push('up')
+			}
+		}
+	}
+
+	return actions
+}
+
+
+function startAstar(state){
+	const searchGrid = initSearchGrid(state)
+
+	const graph = new astar.Graph(searchGrid)
+
+	const players = state.players
+	const goals = state.winnables
+
+	// initialize array to store  path of GridNodes
+	let astarResult = []
+	// initialize array to store final path as possActions
+	let actions = []
+
+	for (let player of players) {
+		for (let goal of goals) {
+			const start = graph.grid[player.y][player.x]
+			const end = graph.grid[goal.y][goal.x]
+			// returns a list of GridNodes
+			astarResult = astar.astar.search(graph, start, end)
+			actions = convertResultToActions(astarResult)
+			
+			// stop if a solution has been found
+			if (actions != []) {
+				break
+			}
+		}
+	}
+
+	return actions	
+	
+
+	// debug prints
+	// console.log("\n### MAP ####")
+	// for (let row of state.orig_map) {
+	// 	console.log(row + " ")
+	// }
+	// console.log("\n### SEARCH GRID ###")
+	// for (let row of searchGrid) {
+	// 	console.log(row + " ")
+	// }
+	// console.log("\n### PLAYERS ###")
+	// for (let player of players) {
+	// 	console.log("(" + player.x  + "," + player.y + ")")
+	// }
+	// console.log("\n### GOALS ###")
+	// for (let goal of goals) {
+	// 	console.log("(" + goal.x  + "," + goal.y + ")")
+	// }
+	// console.log("\n### ASTAR RESULT ###")
+	// console.log(astarResult)
+
+	// console.log("\n### ACTIONS ###")
+	// console.log(actions)
+}
+
+
+
+// VISIBLE FUNCTION FOR OTHER JS FILES (NODEJS)
+module.exports = (state) => startAstar(state);					// returns closest solution in case of timeout
